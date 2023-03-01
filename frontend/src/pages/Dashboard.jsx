3d81acc7a9b921512104285cb0c8abe2e22e749c
stefan <svsfarti@gmail.com>
2023-03-01 11:22:16 +0200

    Bug fixed the route for the post request of the todo

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -26,7 +26,7 @@ function Dashboard() {
         return () => {
             dispatch(reset())
         }
-    }, [user, navigate, isError, message. dispatch]);
+    }, [user, navigate, isError, message, dispatch]);
 
     if (isLoading) {
         return <Spinner />
